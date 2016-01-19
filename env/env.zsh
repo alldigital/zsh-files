59a4fc025fc37771c501cafa65b742bd789681a1
alldigital <edward@tulupnikov.com>
2016-01-19 11:04:28 +0300

    Revert "Android dev for arch tweaks - ANDROID_HOME"
    This reverts commit 9eafa6526e97a53c74f1a9ca59521c5c8aea1e3e.

--- env/env.zsh
+++ env/env.zsh
@@ -10,7 +10,6 @@ if [[ $CURRENT_OS == 'OS X' ]]; then
     # OS X Env Variables
 elif [[ $CURRENT_OS == 'Linux' ]]; then
     # Linux Env Variables
-    export ANDROID_HOME=/opt/android-sdk
 
     if [[ $USER == 'ed' ]]; then
 #        export ECLIPSE_HOME=/opt/eclipse/
