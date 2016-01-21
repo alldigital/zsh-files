48086bc3cac295ac40f46ec80c36d80a3e5fb7d1
alldigital <edward@tulupnikov.com>
2016-01-21 11:34:48 +0300

    Clean up

--- env/env.zsh
+++ env/env.zsh
@@ -6,7 +6,6 @@
 # OS Specific Env Variables
 #
 
-<<<<<<< HEAD
 # Ensure languages are set
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
@@ -16,11 +15,3 @@ export LC_ALL=en_US.UTF-8
 # Ensure editor is set
 export EDITOR=vim
 export VISUAL=vim
-=======
-if [[ $CURRENT_OS == 'OS X' ]]; then
-    # OS X Env Variables
-elif [[ $CURRENT_OS == 'Linux' ]]; then
-    # Linux Env Variables
-    export ANDROID_HOME=/opt/android-sdk
->>>>>>> parent of 59a4fc0... Revert "Android dev for arch tweaks - ANDROID_HOME"
-
---
+++ options/zsh.zsh
@@ -0,0 +1,27 @@
+# Hisory handling
+
+export HISTFILE=~/.zsh/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=$HISTSIZE
+
+# Appends every command to the history file once it is executed
+setopt inc_append_history
+# Reloads the history whenever you use it
+setopt share_history
+setopt extended_history
+# enable '!' history expansion
+setopt bang_hist
+# when exiting, append history entries to $HISTFILE, rather than replacing the old file; this is the default
+setopt append_history
+# save additional info to $HISTFILE
+setopt extended_history
+# if the commandline starts with a whitespace, don't add it to history
+setopt hist_ignore_space
+# append every single command to $HISTFILE immediately after hitting ENTER.
+setopt inc_append_history
+# always import new commands from $HISTFILE (see 'inc_append_history' above)
+setopt share_history
+
+# Audo cd without cd
+setopt autocd
+
