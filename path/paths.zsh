e1e11060961323baf599270910dc652fab7de2c2
alldigital <edward@tulupnikov.com>
2016-05-02 11:15:44 +0300

    Unnecessary IDE paths removed

--- path/paths.zsh
+++ path/paths.zsh
@@ -9,10 +9,10 @@ export PATH=$PATH:$HOME/bin
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # IDE
-export PATH=$PATH:$HOME/netbeans/bin
-export PATH=$PATH:$HOME/idea/bin
-export PATH=$PATH:/data/tmcbeans/bin
-export PATH=$PATH:$HOME/dev/android-studio/bin
-export PATH=$PATH:$HOME/eclipse
+# export PATH=$PATH:$HOME/netbeans/bin
+# export PATH=$PATH:$HOME/idea/bin
+# export PATH=$PATH:/data/tmcbeans/bin
+# export PATH=$PATH:$HOME/dev/android-studio/bin
+# export PATH=$PATH:$HOME/eclipse
 
-export PATH=$PATH:$HOME/.rvm/bin
+# export PATH=$PATH:$HOME/.rvm/bin
