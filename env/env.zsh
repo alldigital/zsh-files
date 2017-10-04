65717746af7c662ae59adc2c26c55fe914634172
alldigital <edward@tulupnikov.com>
2017-10-04 23:25:18 +0300

    Delete LC_ALL env variable

--- env/env.zsh
+++ env/env.zsh
@@ -12,7 +12,7 @@ export XDG_CONFIG_HOME=$HOME/.config
 # Ensure languages are set
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+# export LC_ALL=en_US.UTF-8
 
 # Ensure editor is set
 export EDITOR=vim
