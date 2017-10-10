83601db141c2c110e417ac7d68883a395b7fa4d5
alldigital <edward@tulupnikov.com>
2017-10-10 19:34:52 +0300

    Add guix packages to $PATH

--- guix/guix.zsh
+++ guix/guix.zsh
@@ -1,3 +1,4 @@
 export PATH="$HOME/.guix-profile/bin:$HOME/.guix-profile/sbin${PATH:+:}$PATH"
 export BASH_LOADABLES_PATH="$HOME/.guix-profile/lib/bash${BASH_LOADABLES_PATH:+:}$BASH_LOADABLES_PATH"
 export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
+export GUIX_PACKAGE_PATH=$HOME/guix/packages
