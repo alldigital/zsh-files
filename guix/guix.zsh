852b15dd749d9b853dfb53e763503179a337c826
alldigital <edward@tulupnikov.com>
2017-10-14 22:02:04 +0300

    Add some more paths to guix env

--- guix/guix.zsh
+++ guix/guix.zsh
@@ -2,3 +2,7 @@ export PATH="$HOME/.guix-profile/bin:$HOME/.guix-profile/sbin${PATH:+:}$PATH"
 export BASH_LOADABLES_PATH="$HOME/.guix-profile/lib/bash${BASH_LOADABLES_PATH:+:}$BASH_LOADABLES_PATH"
 export GUIX_LOCPATH=$HOME/.guix-profile/lib/locale
 export GUIX_PACKAGE_PATH=$HOME/guix/packages
+
+export C_INCLUDE_PATH="/home/ed/.guix-profile/include${C_INCLUDE_PATH:+:}$C_INCLUDE_PATH"
+export CPLUS_INCLUDE_PATH="/home/ed/.guix-profile/include${CPLUS_INCLUDE_PATH:+:}$CPLUS_INCLUDE_PATH"
+export LIBRARY_PATH="/home/ed/.guix-profile/lib${LIBRARY_PATH:+:}$LIBRARY_PATH"
