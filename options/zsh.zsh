9b47ffeb5c42434d38f6da9103695d95e4c9231c
alldigital <edward@tulupnikov.com>
2018-03-05 16:32:17 +0300

    WIP - zsh cleanup

--- options/zsh.zsh
+++ options/zsh.zsh
@@ -1,8 +1,12 @@
 # Hisory handling
 
+GREP_EXCLUDE_DIR="{.git,.sass-cache,artwork,node_modules,vendor}"
+OS=`uname`
+
 export HISTFILE=~/.zsh_history
 export HISTSIZE=10000
 export SAVEHIST=$HISTSIZE
+export CLICOLOR=1
 
 # Appends every command to the history file once it is executed
 setopt inc_append_history
@@ -24,4 +28,3 @@ setopt share_history
 
 # Audo cd without cd
 setopt autocd
-
