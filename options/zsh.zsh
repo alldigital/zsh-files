fb229fbe1bf374a63febcb67f8222a94885c3cc6
alldigital <edward@tulupnikov.com>
2018-09-10 10:06:49 +0300

    Change zsh prompt to minimalistic.

--- options/zsh.zsh
+++ options/zsh.zsh
@@ -29,3 +29,31 @@ setopt auto_menu
 setopt hist_ignore_dups
 setopt hist_verify
 setopt interactivecomments
+
+
+# PROMPT
+
+# Enable prompt customization
+setopt prompt_subst
+
+# Colour variables to make prompt theme more readable
+export black=$'%{\e[0;30m%}'
+export red=$'%{\e[0;31m%}'
+export green=$'%{\e[0;32m%}'
+export brown=$'%{\e[0;33m%}'
+export blue=$'%{\e[0;34m%}'
+export purple=$'%{\e[0;35m%}'
+export cyan=$'%{\e[0;36m%}'
+export light_gray=$'%{\e[0;37m%}'
+export dark_gray=$'%{\e[1;30m%}'
+export light_red=$'%{\e[1;31m%}'
+export light_green=$'%{\e[1;32m%}'
+export yellow=$'%{\e[1;33m%}'
+export light_blue=$'%{\e[1;34m%}'
+export pink=$'%{\e[1;35m%}'
+export light_cyan=$'%{\e[1;36m%}'
+export white=$'%{\e[1;37m%}'
+export reset_color=$'%{\e[0m%}'
+
+
+PROMPT='${green}%n@%m${reset_color}>%~ %(!.#.λ) '
